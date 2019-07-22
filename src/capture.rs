ce83d9f1d1d951827c9eaf52eabec64fdac08536
Pierre Chifflier <chifflier@wzdftpd.net>
2019-07-22 23:02:56 +0200

    Add generic method to get a streaming iterator for both pcap and pcap-ng

--- src/capture.rs
+++ src/capture.rs
@@ -1,5 +1,11 @@
 use crate::blocks::PcapBlock;
+use crate::capture_pcap::LegacyPcapReader;
+use crate::capture_pcapng::PcapNGReader;
 use crate::linktype::Linktype;
+use crate::pcap::parse_pcap_header;
+use crate::pcapng::parse_sectionheaderblock;
+use crate::traits::PcapReaderIterator;
+use std::io::BufRead;
 
 /// Generic interface for PCAP or PCAPNG file access
 pub trait Capture {
@@ -8,4 +14,27 @@ pub trait Capture {
     fn get_snaplen(&self) -> u32;
 
     fn iter<'a>(&'a self) -> Box<Iterator<Item = PcapBlock> + 'a>;
+}
+
+/// Get a generic PcapReaderIterator, given a buffered input. The input is probed for pca-ng first,
+/// then pcap.
+pub fn create_reader<'b, B>(
+    mut buffered: B,
+) -> Result<Box<PcapReaderIterator<B> + 'b>, nom::ErrorKind<u32>>
+where
+    B: BufRead + 'b,
+{
+    if let Ok(buffer) = buffered.fill_buf() {
+        // first check for pcapng
+        if let Ok(_) = parse_sectionheaderblock(&buffer) {
+            PcapNGReader::new(buffered).map(|reader| Box::new(reader) as Box<PcapReaderIterator<B>>)
+        } else if let Ok(_) = parse_pcap_header(&buffer) {
+            LegacyPcapReader::new(buffered)
+                .map(|reader| Box::new(reader) as Box<PcapReaderIterator<B>>)
+        } else {
+            Err(nom::ErrorKind::Tag)
+        }
+    } else {
+        Err(nom::ErrorKind::Custom(0))
+    }
 }
