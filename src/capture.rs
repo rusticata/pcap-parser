a81afe1334701f92e20c79cc92909657802df69b
Pierre Chifflier <chifflier@wzdftpd.net>
2021-01-15 09:31:37 +0100

    Improve error handling in create_reader (if input is empty or incomplete)

--- src/capture.rs
+++ src/capture.rs
@@ -39,15 +39,19 @@ where
 {
     let mut buffer = Buffer::with_capacity(capacity);
     let sz = reader.read(buffer.space()).or(Err(PcapError::ReadError))?;
+    if sz == 0 {
+        return Err(PcapError::Eof);
+    }
     buffer.fill(sz);
     // just check that first block is a valid one
     if parse_sectionheaderblock(buffer.data()).is_ok() {
-        PcapNGReader::from_buffer(buffer, reader)
-            .map(|r| Box::new(r) as Box<dyn PcapReaderIterator<R>>)
-    } else if parse_pcap_header(buffer.data()).is_ok() {
-        LegacyPcapReader::from_buffer(buffer, reader)
-            .map(|r| Box::new(r) as Box<dyn PcapReaderIterator<R>>)
-    } else {
-        Err(PcapError::HeaderNotRecognized)
+        return PcapNGReader::from_buffer(buffer, reader)
+            .map(|r| Box::new(r) as Box<dyn PcapReaderIterator<R>>);
+    }
+    match parse_pcap_header(buffer.data()) {
+        Ok(_) => LegacyPcapReader::from_buffer(buffer, reader)
+            .map(|r| Box::new(r) as Box<dyn PcapReaderIterator<R>>),
+        Err(nom::Err::Incomplete(_)) => Err(PcapError::Incomplete),
+        _ => Err(PcapError::HeaderNotRecognized),
     }
 }
---
+++ tests/reader.rs
@@ -0,0 +1,25 @@
+use pcap_parser::{create_reader, PcapError};
+
+#[test]
+fn test_empty_reader_error() {
+    let empty: &[u8] = &[];
+    let res = create_reader(1024, empty);
+    assert!(res.is_err());
+    if let Err(err) = res {
+        assert_eq!(err, PcapError::Eof);
+    } else {
+        unreachable!();
+    }
+}
+
+#[test]
+fn test_empty_reader_incomplete() {
+    let empty: &[u8] = &[0];
+    let res = create_reader(1024, empty);
+    assert!(res.is_err());
+    if let Err(err) = res {
+        assert_eq!(err, PcapError::Incomplete);
+    } else {
+        unreachable!();
+    }
+}
