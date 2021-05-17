dcead2f94810b51febb1786f58ecccf601b7a16f
Pierre Chifflier <chifflier@wzdftpd.net>
2021-05-17 17:46:46 +0200

    Report error instead of EOF when a block is incomplete and no more data is available

--- src/capture_pcapng.rs
+++ src/capture_pcapng.rs
@@ -138,7 +138,11 @@ where
     R: Read,
 {
     fn next(&mut self) -> Result<(usize, PcapBlockOwned), PcapError> {
-        if self.buffer.position() == 0 && self.buffer.available_data() == 0 || self.reader_exhausted
+        // Return EOF if
+        // 1) all bytes have been read
+        // 2) no more data is available
+        if self.buffer.available_data() == 0
+            && (self.buffer.position() == 0 || self.reader_exhausted)
         {
             return Err(PcapError::Eof);
         }
