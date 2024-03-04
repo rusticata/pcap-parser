a6e6744688c2f33e273c4e974dc62899d354fb2c
David Ibbitson <dibbitson@anduril.com>
2024-03-04 07:11:47 -0800

    format fix

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -30,7 +30,7 @@ pub use crate::data::pcap_nflog::*;
 use crate::linktype::Linktype;
 use crate::read_u32_e;
 use nom::number::complete::be_u32;
-use nom::number::streaming::{be_u8, be_u16, be_u64};
+use nom::number::streaming::{be_u16, be_u64, be_u8};
 use nom::IResult;
 
 pub const ETHERTYPE_IPV4: u16 = 0x0800;
